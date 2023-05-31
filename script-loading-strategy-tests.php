8f763ad322ee2274245fbeaf2ef37c638a5a961d
Weston Ruter <westonruter@google.com>
2023-05-30 17:50:10 -0700

    Add phpdoc block

--- script-loading-strategy-tests.php
+++ script-loading-strategy-tests.php
@@ -1,11 +1,32 @@
 <?php
 /**
+ * Optimize Loading Separate Core Block Assets in Classic Themes Plugin.
+ *
+ * @package OptimizeLoadingSeparateCoreBlockAssetsInClassicThemes
+ * @author Weston Ruter
+ * @link https://github.com/westonruter/wp-script-loading-strategy-tests
+ * @license GPL-2.0-or-later
+ * @copyright 2023 Google Inc.
+ *
+ * @wordpress-plugin
  * Plugin Name: Script Loading Strategy Tests
+ * Plugin URI: https://github.com/westonruter/wp-script-loading-strategy-tests
+ * Description: Demo plugin that puts together various scenarios to test the script loading strategies proposed for WordPress core.
+ * Version: 0.1.0
+ * Author: Weston Ruter
+ * Author URI: https://weston.ruter.net/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Requires at least: 5.8
+ * Requires PHP: 5.6
+ * Update URI: false
  */
+
+namespace ScriptLoadingStrategyTests;
 
 add_action( 'wp_enqueue_scripts', static function () {
 
-	foreach ( [ 'blocking', 'async', 'defer' ] as $strategy ) {
+	foreach ( [ '', 'async', 'defer' ] as $strategy ) {
 
 		wp_enqueue_script(
 			$strategy,
