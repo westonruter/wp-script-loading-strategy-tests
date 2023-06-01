a93402120ac5662741b69e7924908bbfa809cd7f
Weston Ruter <westonruter@google.com>
2023-06-01 15:30:10 -0700

    Add note to snapshot about failing variant

--- cases/blocking-with-async-dependency.php
+++ cases/blocking-with-async-dependency.php
@@ -7,6 +7,20 @@ add_action( 'wp_enqueue_scripts', static function () {
 } );
 
 // Snapshot of output below:
+/*
+ * Note: This was sometimes result in a snapshot as follows:
+ *
+ *   blocking-not-async-without-dependency: before inline
+ *   blocking-not-async-without-dependency: script
+ *   blocking-not-async-without-dependency: after inline
+ *   async-with-blocking-dependency: script
+ *   async-with-blocking-dependency: after inline
+ *   async-with-blocking-dependency: before inline
+ *
+ * The before script for async-with-blocking-dependency is getting executed _after_ the after script. This appears to
+ * have been fixed with the introduction of \WP_Scripts::should_delay_inline_script() which prevents delaying a before
+ * script when there are blocking dependencies.
+ */
 ?>
 blocking-not-async-without-dependency: before inline
 blocking-not-async-without-dependency: script
