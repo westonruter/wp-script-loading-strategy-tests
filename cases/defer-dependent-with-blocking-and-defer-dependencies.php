182a61bd88dd16844e5f07bcb850b0f13b4abac1
Weston Ruter <westonruter@google.com>
2023-06-05 17:16:26 -0700

    Add  defer-dependent-with-blocking-and-defer-dependencies test case

---
+++ cases/defer-dependent-with-blocking-and-defer-dependencies.php
@@ -0,0 +1,20 @@
+<?php
+namespace ScriptLoadingStrategyTests;
+
+add_action( 'wp_enqueue_scripts', static function () {
+	enqueue_test_script( 'blocking-dependency-with-defer-following-dependency', 'blocking', [] );
+	enqueue_test_script( 'defer-dependency-with-blocking-preceding-dependency', 'defer', [] );
+	enqueue_test_script( 'defer-dependent-of-blocking-and-defer-dependencies', 'defer', [ 'blocking-dependency-with-defer-following-dependency', 'defer-dependency-with-blocking-preceding-dependency' ] );
+} );
+
+// Snapshot of output below:
+?>
+blocking-dependency-with-defer-following-dependency: before inline
+blocking-dependency-with-defer-following-dependency: script
+blocking-dependency-with-defer-following-dependency: after inline
+defer-dependency-with-blocking-preceding-dependency: before inline
+defer-dependency-with-blocking-preceding-dependency: script
+defer-dependency-with-blocking-preceding-dependency: after inline
+defer-dependent-of-blocking-and-defer-dependencies: before inline
+defer-dependent-of-blocking-and-defer-dependencies: script
+defer-dependent-of-blocking-and-defer-dependencies: after inline
